fe4157173636399bb8ca759dc632095499271e96
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-01-14 16:37:50 -0200

    ddddd

--- src/main/java/br/com/senaigo/helpdesk/HelpDeskApplication.java
+++ src/main/java/br/com/senaigo/helpdesk/HelpDeskApplication.java
@@ -1,17 +1,23 @@
 package br.com.senaigo.helpdesk;
 
+import java.util.Locale;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 import br.com.senaigo.helpdesk.api.entity.User;
 import br.com.senaigo.helpdesk.api.enumeration.ProfileEnum;
 import br.com.senaigo.helpdesk.api.repository.UserRepository;
 
-@SpringBootApplication
-public class HelpDeskApplication{
+@SpringBootApplication(scanBasePackages = { "br.com.senaigo.helpdesk" })
+public class HelpDeskApplication extends SpringBootServletInitializer {
 	
 	private static final String E_MAIL = "suleimanmoraes@yahoo.com";
 	private static final String PASSWORD = "123456";
@@ -38,5 +44,15 @@ public class HelpDeskApplication{
 			userRepository.save(admin);
 			System.out.println(String.format("Criando Usuário Admin, com E-mail/Login == \"%s\" e Senha == \"%s\".", E_MAIL, PASSWORD));
 		}
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelpDeskApplication.class);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 }
